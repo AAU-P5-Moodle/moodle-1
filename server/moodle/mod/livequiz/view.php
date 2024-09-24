aa1ad04acd5f1d67e22d6e9fc9effb703d81cd20
EmilChri <113058804+EmilChri@users.noreply.github.com>
2024-09-24 11:54:09 +0200

    the activity now displays "Live Quiz" as the name of the activity

--- server/moodle/mod/livequiz/view.php
+++ server/moodle/mod/livequiz/view.php
@@ -5,8 +5,8 @@ require_once('../../config.php');
 $id = required_param('id', PARAM_INT); // Course module ID
 
 $PAGE->set_url('/mod/livequiz/view.php', array('id' => $id));
-$PAGE->set_title(get_string('liveQuiz', 'mod_livequiz'));
-$PAGE->set_heading(get_string('live_Quiz', 'mod_livequiz'));
+$PAGE->set_title(get_string('modulename', 'mod_livequiz'));
+$PAGE->set_heading(get_string('modulename', 'mod_livequiz'));
 
 echo $OUTPUT->header();
 echo $OUTPUT->heading('Live Quiz');
