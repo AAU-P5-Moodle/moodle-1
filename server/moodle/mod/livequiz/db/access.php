7250784ff9877e6ca2c2d5442aaeeb7c9b7b01ec
EmilChri <113058804+EmilChri@users.noreply.github.com>
2024-09-24 09:39:48 +0200

    Create access.php

---
+++ server/moodle/mod/livequiz/db/access.php
@@ -0,0 +1,35 @@
+<?php
+
+
+defined('MOODLE_INTERNAL') || die();
+
+
+$capabilities = [
+
+    //Allows all roles to view the livequiz
+    'mod/quiz:view' => [
+        'captype' => 'read',
+        'contextlevel' => CONTEXT_MODULE,
+        'archetypes' => [
+            'guest' => CAP_ALLOW,
+            'student' => CAP_ALLOW,
+            'teacher' => CAP_ALLOW,
+            'editingteacher' => CAP_ALLOW,
+            'manager' => CAP_ALLOW
+        ]
+    ],
+
+    // Functionality to add the activity to course
+    'mod/quiz:addinstance' => [
+        'riskbitmask' => RISK_XSS,
+
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_COURSE,
+        'archetypes' => [
+            'editingteacher' => CAP_ALLOW,
+            'manager' => CAP_ALLOW
+        ],
+        'clonepermissionsfrom' => 'moodle/course:manageactivities'
+    ]
+
+];
