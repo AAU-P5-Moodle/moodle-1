40a38871190cbb15215e5c519106f93d5dab1463
Peterrahr <Prni22@student.aau.dk>
2024-09-25 12:03:43 +0200

    changed settings for admin

--- server/moodle/mod/livequiz/settings.php
+++ server/moodle/mod/livequiz/settings.php
@@ -4,10 +4,10 @@ global $ADMIN;
 defined('MOODLE_INTERNAL') || die();
 
 if ($ADMIN->fulltree) {
-    $settings = new admin_settingpage('mod_livequiz', get_string('pluginname', 'mod_livequiz'));
+    $settings->add(new admin_settingpage('mod_livequiz', get_string('pluginname', 'mod_livequiz')));
 
     $settings->add(new admin_setting_configtext('livequiz/some_setting',
         get_string('somesetting', 'mod_livequiz'), get_string('somesetting_desc', 'mod_livequiz'), 'default_value'));
-
+    
     $ADMIN->add('modsettings', $settings);
 }
