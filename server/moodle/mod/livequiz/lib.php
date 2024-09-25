73424fb1a7e598a5a95ed4e223fba94fd4072962
EmilChri <113058804+EmilChri@users.noreply.github.com>
2024-09-25 09:53:56 +0200

    lib file populated

--- server/moodle/mod/livequiz/lib.php
+++ server/moodle/mod/livequiz/lib.php
@@ -1,3 +1,34 @@
 <?php
 
 defined('MOODLE_INTERNAL') || die();
+
+function livequiz_add_instance($quizdata){
+    global $DB;
+
+    $quizdata->timecreated = time();
+    $quizdata->timemodified = time();
+
+    $quizdata->id = $DB->insert_record('livequiz', $quizdata);
+
+    return $quizdata->id;
+}
+
+function livequiz_update_instance($quizdata){
+    global $DB;
+
+    $quizdata->timemodified = time();
+    $quizdata->id = $quizdata->instance;
+
+    $DB->update_record('livequiz', $quizdata);
+
+    return true;
+}
+
+
+function livequiz_delete_instance($id){
+    global $DB;
+
+    $DB->delete_records('livequiz', ['id' => $id]);
+
+    return true;
+}
