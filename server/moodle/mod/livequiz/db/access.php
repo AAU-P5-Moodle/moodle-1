63719203f6916f64b5fcc08f5967f421d9db9991
Peterrahr <Prni22@student.aau.dk>
2024-09-26 12:12:34 +0200

    Modform included
    Modform included from moodle docs.
    Boilerplate code inserted

--- server/moodle/mod/livequiz/db/access.php
+++ server/moodle/mod/livequiz/db/access.php
@@ -1,12 +1,25 @@
 <?php
-
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 defined('MOODLE_INTERNAL') || die();
 
 
 $capabilities = [
 
-    //Allows all roles to view the livequiz
+    //Allows all roles to view the livequiz.
     'mod/livequiz:view' => [
         'captype' => 'read',
         'contextlevel' => CONTEXT_MODULE,
@@ -15,11 +28,11 @@ $capabilities = [
             'student' => CAP_ALLOW,
             'teacher' => CAP_ALLOW,
             'editingteacher' => CAP_ALLOW,
-            'manager' => CAP_ALLOW
-        ]
+            'manager' => CAP_ALLOW,
+        ],
     ],
 
-    // Functionality to add the activity to course
+    // Functionality to add the activity to course.
     'mod/livequiz:addinstance' => [
         'riskbitmask' => RISK_XSS,
 
@@ -27,9 +40,7 @@ $capabilities = [
         'contextlevel' => CONTEXT_COURSE,
         'archetypes' => [
             'editingteacher' => CAP_ALLOW,
-            'manager' => CAP_ALLOW
-        ],
-        'clonepermissionsfrom' => 'moodle/course:manageactivities'
-    ]
-
+            'manager' => CAP_ALLOW,        ],
+        'clonepermissionsfrom' => 'moodle/course:manageactivities',
+    ],
 ];
