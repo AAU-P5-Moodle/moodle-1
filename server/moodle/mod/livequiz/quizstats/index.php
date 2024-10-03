3439b342249e2aad9c32dd2b8a9cb7669373ca13
Deekahy <Lennart.Diego.Kahn@gmail.com>
2024-10-03 20:53:35 +0200

    Revert "Switched to a non advertising temp image"
    This reverts commit 289076e5404262d1b5a59803e495cc95a5f26d2e.

--- server/moodle/mod/livequiz/quizstats/index.php
+++ server/moodle/mod/livequiz/quizstats/index.php
@@ -17,13 +17,13 @@ echo $OUTPUT->header();
 echo '<div style="width:100%;">';
 echo '<div class="quiz-stats-chart" style="width:30%; margin:auto;">';
 echo '<h2>Quiz Performance Overview</h2>';
-echo '<img src="https://dummyimage.com/600x400" alt="Demo Chart">';
+echo '<img src="https://via.placeholder.com/600x400?text=Demo+Chart+1" alt="Demo Chart 1">';
 echo '<br>';
-echo '<img src="https://dummyimage.com/600x400" alt="Demo Chart">';
+echo '<img src="https://via.placeholder.com/600x400?text=Demo+Chart+2" alt="Demo Chart 2">';
 echo '<br>';
-echo '<img src="https://dummyimage.com/600x400" alt="Demo Chart">';
+echo '<img src="https://via.placeholder.com/600x400?text=Demo+Chart+3" alt="Demo Chart 3">';
 echo '<br>';
-echo '<img src="https://dummyimage.com/600x400" alt="Demo Chart">';
+echo '<img src="https://via.placeholder.com/600x400?text=Demo+Chart+4" alt="Demo Chart 4">';
 echo '<br>';
 echo '</div>';
 
