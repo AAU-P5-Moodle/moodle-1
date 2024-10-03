200cd121d34c8696914e2845ec42be5f7b12d5a8
Thomas Andersen <55490372+Thekid2002@users.noreply.github.com>
2024-10-03 11:41:39 +0200

    the docker setup

---
+++ livequiz/lang/en/livequiz.php
@@ -0,0 +1,24 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+/**
+ * Strings for component 'livequiz', language 'en'
+ *
+ * @package   mod_livequiz
+ */
+// These strings are used in the livequiz module.
+// The strings in the brackets will be replaced by the actual values.
+$string['modulename'] = 'Live Quiz';
+$string['modulenameplural'] = 'Live Quizzes';
+$string['modulename_help'] = 'Use the live quiz module to create interactive quizzes.';
+$string['livequiz:addinstance'] = 'Add a new live quiz';
+$string['livequiz:submit'] = 'Submit live quiz';
+$string['livequiz:view'] = 'View live quiz';
+$string['pluginadministration'] = 'Live quiz administration';
+$string['pluginname'] = 'Live Quiz';
+$string['somesetting'] = 'Some setting';
+$string['somesetting_desc'] = 'Description of some setting';
