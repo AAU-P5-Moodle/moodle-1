e0cddd5edd09285c62558b3e8e3e7707e3d264f6
Alexander Winther Moldt <awmoldt.dev@gmail.com>
2024-10-14 11:11:08 +0200

    Update script.js
    changed all danish comments to english

--- server/moodle/mod/livequiz/script.js
+++ server/moodle/mod/livequiz/script.js
@@ -3,39 +3,39 @@ const path = require('path');
 const uglifyJS = require('uglify-js');
 
 /*
-* THIS IS A TEMPORARY SCRIPT. A PROPER SOLUTION SHOULD BE IMPLEMENTED WITH GRUNT
-* Dette script skal køres når du ønsker at se ændringer fra dit js.
+* THIS IS A TEMPORARY SCRIPT. A PROPER SOLUTION SHOULD BE IMPLEMENTED WITH GRUNT.
+* This script needs to run when you want to see changes from your js. 
 *
 * */
 
 
 
 
-// Mapper
+// folders.
 const srcDir = path.join(__dirname, 'amd', 'src');
 const buildDir = path.join(__dirname, 'amd', 'build');
 
-// Opret build-mappen, hvis den ikke findes
+// creates build-folder, if it's isn't found.
 if (!fs.existsSync(buildDir)) {
   fs.mkdirSync(buildDir);
 }
 
-// Læs alle .js-filer fra src-mappen
+// reads all, js-files form the src-folder.
 fs.readdir(srcDir, (err, files) => {
   if (err) {
     console.error("Error reading the src directory:", err);
     return;
   }
 
-  // Filtrer kun .js-filer
+  // filters only .js-files
   files.filter(file => file.endsWith('.js')).forEach(file => {
     const srcFilePath = path.join(srcDir, file);
     const buildFilePath = path.join(buildDir, file.replace('.js', '.min.js'));
 
-    // Læs indholdet af filen
+    // reads the contents of the files
     const code = fs.readFileSync(srcFilePath, 'utf8');
 
-    // Uglify filen
+    // Uglify files
     const result = uglifyJS.minify(code);
 
     if (result.error) {
@@ -43,7 +43,7 @@ fs.readdir(srcDir, (err, files) => {
       return;
     }
 
-    // Skriv den uglified version til build-mappen
+    // writes an uglified version to the build-folder
     fs.writeFileSync(buildFilePath, result.code, 'utf8');
     console.log(`Successfully uglified: ${file} -> ${buildFilePath}`);
   });
