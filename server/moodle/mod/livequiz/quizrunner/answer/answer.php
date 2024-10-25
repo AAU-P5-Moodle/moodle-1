09146181fca9a3879c604c64360220d0b0b43957
ZeroConfidence <113094244+ZeroConfidence@users.noreply.github.com>
2024-10-25 13:05:01 +0200

    updated answer.php to fallow code standard

--- server/moodle/mod/livequiz/quizrunner/answer/answer.php
+++ server/moodle/mod/livequiz/quizrunner/answer/answer.php
@@ -1,4 +1,40 @@
 <?php
+// This file is part of Moodle - http://moodle.org/.
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Interface for answer types in the quiz module.
+ *
+ * This interface defines a method for generating the HTML representation
+ * of an answer in the quiz module.
+ *
+ * @package   mod_livequiz
+ * @copyright 2024 Software AAU
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+/**
+ * Interface answer.
+ *
+ * Represents an interface for different types of answers in the quiz module.
+ */
 interface answer {
-	public function html() : string;
+    /**
+     * Generate the HTML representation of the answer.
+     *
+     * @return string The HTML representation.
+     */
+    public function html(): string;
 }
