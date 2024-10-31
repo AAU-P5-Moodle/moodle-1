bb8ca0f75dc40a4e800055c19350e257ebb13c9f
Alexander Winther Moldt <amoldt22@student.aau.dk>
2024-10-31 20:04:53 +0100

    Update server/moodle/mod/livequiz/index.php
    Co-authored-by: Christoffer Gamél <113096859+ChristofferGamel@users.noreply.github.com>

--- server/moodle/mod/livequiz/index.php
+++ server/moodle/mod/livequiz/index.php
@@ -32,7 +32,6 @@ $id = required_param('id', PARAM_INT);
 // Fetch the requested course.
 $course = $DB->get_record('course', ['id' => $id], '*', MUST_EXIST);
 
-
 // Require that the user is logged into the course.
 require_course_login($course);
 
