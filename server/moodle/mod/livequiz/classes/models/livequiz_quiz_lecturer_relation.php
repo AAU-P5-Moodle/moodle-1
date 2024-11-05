bbdaa79d3dbbc513b88dd6045754714d52f322f7
mantarias <mantas2630@hotmail.com>
2024-11-05 12:42:19 +0100

    added models for the 2 relations tables
    Co-authored-by: CarlHejlesen <CarlHejlesen@users.noreply.github.com>

---
+++ server/moodle/mod/livequiz/classes/models/livequiz_quiz_lecturer_relation.php
@@ -0,0 +1,154 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace mod_livequiz\models;
+
+use dml_exception;
+use dml_transaction_exception;
+
+/**
+ * 'Static' class, do not instantiate.
+ * Displays the livequiz view page.
+ * @package   mod_livequiz
+ * @copyright 2024 Software AAU
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class livequiz_quiz_lecturer_relation {
+    /**
+     *
+     * Append a relation betweeen af lecturer_id and the quiz id. For easy access
+     *
+     * @param int $quizid
+     * @param int $lecturerid
+     * @return void
+     * @throws dml_exception
+     * @throws dml_transaction_exception
+     *
+     */
+    public static function append_lecturer_quiz_relation(int $quizid, int $lecturerid): void {
+        global $DB;
+        $DB->insert_record('livequiz_quiz_lecturer', ['lecturer_id' => $lecturerid, 'quiz_id' => $quizid]);
+    }
+
+    /**
+     *
+     * Gets lecturer relation by lecturer id. Will be used to get all the quizes that relates to that teacher
+     *
+     *
+     * @param int $lecturerid
+     * @return array
+     * @throws dml_exception
+     * @throws dml_transaction_exception
+     *
+     */
+    public static function get_lecturer_quiz_relation_by_lecturer_id(int $lecturerid): array {
+        global $DB;
+        return $DB->get_record('livequiz_quiz_lecturer', ['lecturer_id' => $lecturerid]);
+    }
+
+    /**
+     *
+     * Gets lecturer relation by quiz id. Will be used to get all the teachers that have made that quiz.
+     *
+     *
+     * @param int $quizid
+     * @return array
+     * @throws dml_exception
+     * @throws dml_transaction_exception
+     *
+     */
+    public static function get_lecturer_quiz_relation_by_quiz_id(int $quizid): array {
+        global $DB;
+        return $DB->get_record('livequiz_quiz_lecturer', ['quiz_id' => $quizid]);
+    }
+    /**
+     * returns array with data from livequiz_quiz_lecturer based on relation id
+     * @param int $id
+     * @return array
+     * @throws dml_exception
+     */
+    public static function get_lecturer_quiz_relation_by_id(int $id): array {
+        global $DB;
+        return $DB->get_record('livequiz_quiz_lecturer', ['id' => $id]);
+    }
+
+    /**
+     *
+     * Updates the overner of the quiz. in the relation. So the lecturerId will be updated.
+     *
+     *
+     * @param int $lecturerid
+     * @param int $id
+     * @return void
+     * @throws dml_exception
+     * @throws dml_transaction_exception
+     *
+     */
+    public static function update_lecturer_quiz_relation_lecturer_id(int $id, int $lecturerid): void {
+        global $DB;
+        $DB->update_record('livequiz_quiz_lecturer', ['id' => $id, 'lecturer_id' => $lecturerid]);
+    }
+
+    /**
+     *
+     * Updates the quiz_id using the relation id. Mabey used in the future.
+     *
+     *
+     * @param int $quizid
+     * @param int $id
+     * @return array
+     * @throws dml_exception
+     * @throws dml_transaction_exception
+     *
+     */
+    public static function update_lecturer_quiz_relation_quiz_id(int $id, int $quizid): void {
+        global $DB;
+        $DB->update_record('livequiz_quiz_lecturer', ['id' => $id, 'quiz_id' => $quizid]);
+    }
+
+    /**
+     *
+     * Updates both quiz_id and lecturer_id given a lecturer_quiz_relation_both id.
+     *
+     *
+     * @param int $lecturerid
+     * @param int $id
+     * @param int $quizid
+     * @return void
+     * @throws dml_exception
+     * @throws dml_transaction_exception
+     *
+     */
+    public static function update_lecturer_quiz_relation_both(int $id, int $quizid, int $lecturerid): void {
+        global $DB;
+        $DB->update_record('livequiz_quiz_lecturer', ['id' => $id, 'quiz_id' => $quizid, 'lecturer_id' => $lecturerid]);
+    }
+    /**
+     *
+     * Deletes  lecturer_quiz_relation by the relation id
+     *
+     *
+     * @param int $id
+     * @return void
+     * @throws dml_exception
+     * @throws dml_transaction_exception
+     *
+     */
+    public static function delete_lecturer_quiz_relation_by_id(int $id): void {
+        global $DB;
+        $DB->delete_records('livequiz_quiz_lecturer', ['id' => $id]);
+    }
+}
