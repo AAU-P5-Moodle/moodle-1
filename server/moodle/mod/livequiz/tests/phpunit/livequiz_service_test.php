3cb79b786cc35db89283fa0b4753083980b4e256
Adomas Ciplys <aciply22@student.aau.dk>
2024-11-07 15:43:55 +0100

    Added test for new_participation function

--- server/moodle/mod/livequiz/tests/phpunit/livequiz_service_test.php
+++ server/moodle/mod/livequiz/tests/phpunit/livequiz_service_test.php
@@ -30,18 +30,47 @@ use mod_livequiz\models\livequiz;
 use mod_livequiz\services\livequiz_services;
 use mod_livequiz\models\question;
 use mod_livequiz\models\answer;
+use mod_livequiz\models\participation;
+use mod_livequiz\models\student_quiz_relation;
 
 /**
  * Testing examples for LiveQuiz service.
  */
 final class livequiz_service_test extends \advanced_testcase {
     /**
-     * Set up the test environment.
+     * Create participation test data.
+     * @return array
+     */
+    protected function create_participation_data_for_test(): array {
+        return  $participationdata = [
+            'studentid' => 2,
+            'quizid' => 1,
+        ];
+    }
+
+    /**
+     * Setup before each test.
+     * @return void
      */
     protected function setUp(): void {
         parent::setUp();
         $this->resetAfterTest(true);
     }
 
+    /**
+     * Test of new_participation
+     * @covers \mod_livequiz\services\livequiz_services::new_participation
+     */
+    public function test_new_participation(): void {
+        $participationdata = $this->create_participation_data_for_test();
+        $service = livequiz_services::get_singleton_service_instance();
 
+        $actualstudentid = $participationdata['studentid'];
+        $acutalquizid = $participationdata['quizid'];
+
+        $participation = $service->new_participation($actualstudentid, $acutalquizid);
+        $this->assertInstanceOf(participation::class, $participation);
+        $this->assertEquals($participation->get_studentid(), $actualstudentid);
+        $this->assertEquals($participation->get_livequizid(), $acutalquizid);
+    }
 }
