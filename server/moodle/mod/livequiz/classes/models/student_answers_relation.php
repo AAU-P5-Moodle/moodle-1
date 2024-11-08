e0f7a085dc1b400940bcfae70f7fb800d0f929e7
Adomas Ciplys <aciply22@student.aau.dk>
2024-11-08 19:26:21 +0100

    Created student answers model class

---
+++ server/moodle/mod/livequiz/classes/models/student_answers_relation.php
@@ -0,0 +1,75 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Class representing the relationship between students and answers.
+ * @package   mod_livequiz
+ * @copyright 2024 Software AAU
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace mod_livequiz\models;
+
+use dml_exception;
+
+/**
+ * Class student_answers_relation
+ * @package mod_livequiz\student_answers_relation
+ */
+class Student_answers_relation {
+    /**
+     *  Insert student answer relation. Represents an answer given by a student in a participation.
+     *
+     * @param int $studentid
+     * @param int $answerid
+     * @param int $participationid
+     * @return void
+     * @throws dml_exception
+     */
+    public static function insert_student_answer_relation(int $studentid, int $answerid, int $participationid): void {
+        global $DB;
+        $DB->insert_record('livequiz_student_answers', [
+            'student_id' => $studentid,
+            'answer' => $answerid,
+            'participation_id' => $participationid,
+        ]);
+    }
+
+    /**
+     * Get all answers for a student in a given participation
+     *
+     * @param $quizid int
+     * @return array // An array of question objects.
+     * @throws dml_exception
+     */
+    public static function get_answers_from_student_in_participation(int $studentid, int $participationid): array {
+        global $DB;
+
+        $answerids = $DB->get_records(
+            'livequiz_student_answers',
+            ['student_id' => $studentid, 'participation_id' => $participationid],
+            '',
+            'answer_id'
+        );
+        $answers = [];
+
+        foreach ($answerids as $answerid) {
+            $answers[] = answer::get_answer_from_id($answerid->answer_id);
+        }
+
+        return $answers;
+    }
+}
