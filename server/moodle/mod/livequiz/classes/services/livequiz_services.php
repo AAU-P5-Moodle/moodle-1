5c2cc8a309186cdb092f8ce8a46b55d00ec1f19a
Emil <113058804+EmilChri@users.noreply.github.com>
2024-11-28 11:23:46 +0100

    Update livequiz_services.php

--- server/moodle/mod/livequiz/classes/services/livequiz_services.php
+++ server/moodle/mod/livequiz/classes/services/livequiz_services.php
@@ -383,7 +383,6 @@ class livequiz_services {
         $answers = questions_answers_relation::get_answers_from_question($questionid);
         foreach ($answers as $answer) {
             $currentanswerid = $answer->get_id();
-            questions_answers_relation::delete_question_answer_relation($questionid, $currentanswerid);
             self::delete_answer($currentanswerid, $questionid);
         }
 
